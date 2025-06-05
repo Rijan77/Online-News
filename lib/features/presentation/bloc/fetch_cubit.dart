a31ae12c69f833b1c398ec5af6e0e9b046bab596
Rijan77 <rijanacharya73@gmail.com>
2025-06-05 14:25:53 +0545

    Modified Fetch Cubit

--- lib/features/presentation/bloc/fetch_cubit.dart
+++ lib/features/presentation/bloc/fetch_cubit.dart
@@ -1,15 +1,22 @@
 
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/features/data/api/static_api.dart';
 
 import 'fetch_state.dart';
 
 
 class FetchNewsCubit extends Cubit<FetchNews> {
-  FetchNewsCubit(): super(InitialFetchNews());
+
+  final ApiService _apiService;
+
+  FetchNewsCubit(this._apiService): super(InitialFetchNews());
 
   Future<void> fetchNews() async{
     emit(LoadingFetchNews());
+
+    final service = await _apiService.getAll();
+    emit(SuccessFetchNews());
   }
 
 
