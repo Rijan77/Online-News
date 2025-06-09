39ae137d4d9244b6dbe829654317f855fcd820d4
Rijan77 <rijanacharya73@gmail.com>
2025-06-09 10:45:50 +0545

    Intigrate database

---
+++ lib/database/database_helper.dart
@@ -0,0 +1,43 @@
+import 'package:news_app/database/user_favorite.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper{
+  static final DatabaseHelper instance= DatabaseHelper._instance();
+  static Database? _database;
+
+  DatabaseHelper._instance();
+
+  Future<Database> get db async{
+    _database ??= await initDb();
+    return _database!;
+  }
+
+  Future<Database> initDb() async{
+    String databasePath = await getDatabasesPath();
+    String path = join(databasePath, 'online_news.db');
+
+    return await openDatabase(path, version: 1,  onCreate: _onCreate);
+  }
+
+  Future _onCreate (Database db, int version)async{
+    await db.execute('''CREATE TABLE user_favorite(
+    id INTEGER PRIMARY KEY,
+    email TEXT,
+    isFavorite NUMERIC
+    )'''
+    );
+
+  }
+
+  Future<int> insertUser(UserFavorite userFavorite) async {
+    Database db = await instance.db;
+    return await db.insert('user_favorite', userFavorite.toMap());
+  }
+
+  Future<List<Map<String, dynamic>>> queryAllUsers() async {
+    Database db = await instance.db;
+    return await db.query('user_favorite');
+  }
+
+}
---
+++ lib/database/user_favorite.dart
@@ -0,0 +1,20 @@
+
+class UserFavorite{
+  final int? id;
+  final String email;
+  final bool? isFavorite;
+
+  UserFavorite({this.id, required this.email, this.isFavorite});
+
+  Map<String, dynamic> toMap(){
+    return {'id': id, 'email': email, 'isFavorite': isFavorite};
+  }
+
+  factory UserFavorite.fromMap(Map<String, dynamic> map){
+    return UserFavorite(
+      id: map['id'],
+      email: map['email'],
+      isFavorite: map['isFavorite']
+    );
+  }
+}
