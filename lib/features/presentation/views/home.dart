e53bbc668abb98c86a69b8716a16abbbfc37aeeb
Rijan77 <rijanacharya73@gmail.com>
2025-06-12 15:44:24 +0545

    Making Homepage Responsive

--- lib/features/presentation/views/home.dart
+++ lib/features/presentation/views/home.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/features/presentation/views/widgets/home_layouts.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
+import '../../data/api/model_api.dart';
 import '../bloc/fetch_cubit.dart';
 import '../bloc/fetch_state.dart';
 
@@ -11,6 +13,8 @@ class Home extends StatefulWidget {
 
   @override
   State<Home> createState() => _HomeState();
+
+
 }
 
 class _HomeState extends State<Home> {
@@ -28,23 +32,38 @@ class _HomeState extends State<Home> {
     });
   }
 
+
+
+
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
+    final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
 
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: isPortrait
+            ? kToolbarHeight
+            : kToolbarHeight * 0.5,
         backgroundColor: Colors.blueGrey.shade200,
+
         leading: InkWell(
           onTap: () {
             Navigator.pop(context);
           },
-          child: const Icon(Icons.arrow_back, size: 30),
+          child:  Icon(Icons.arrow_back,
+            size: isPortrait? 35: 25
+          ),
         ),
-        title: const Center(
-          child: Text(
-            "Online News",
-            style: TextStyle(fontWeight: FontWeight.w700),
+        title: Center(
+          child: Expanded(
+            child: Text(
+              "Online News",
+              style: TextStyle(fontWeight: isPortrait? FontWeight.w700: FontWeight.bold,
+                fontSize: isPortrait? 20: 20,
+              ),
+
+            ),
           ),
         ),
         actions: [
@@ -60,29 +79,31 @@ class _HomeState extends State<Home> {
                       await cubit.refreshFavorites();
                       await cubit.fetchNews();
                     },
-                    icon: const Icon(Icons.favorite, size: 35),
+                    icon: Icon(Icons.favorite, size: isPortrait? 30: 20),
                   ),
                   if (cubit.favoriteCount > 0)
                     Positioned(
-                      right: 2,
-                      top: 2,
+                      right: isPortrait? 2: 1,
+                      top: isPortrait? 2: 0,
                       child: Container(
-                        padding: const EdgeInsets.all(2),
+                        padding: EdgeInsets.all(isPortrait? 2: 0),
                         decoration: BoxDecoration(
                           color: Colors.red,
-                          borderRadius: BorderRadius.circular(10),
+                          borderRadius: BorderRadius.circular(isPortrait? 30: 80),
                         ),
-                        constraints: const BoxConstraints(
-                          minWidth: 20,
-                          minHeight: 20,
+                        constraints: BoxConstraints(
+                          minHeight: isPortrait ? 20 : 15,
+                          minWidth: isPortrait ? 20 : 15,
                         ),
-                        child: Text(
-                          cubit.favoriteCount.toString(),
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
+                        child: Center(
+                          child: Text(
+                            cubit.favoriteCount.toString(),
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: isPortrait? 12: 8,
+                            ),
+                            textAlign: TextAlign.center,
                           ),
-                          textAlign: TextAlign.center,
                         ),
                       ),
                     ),
@@ -104,108 +125,7 @@ class _HomeState extends State<Home> {
             final news = state.newModel;
             final cubit = context.read<FetchNewsCubit>();
 
-            return RefreshIndicator(
-              onRefresh: () async {
-                await cubit.fetchNews();
-                await cubit.updateFavoriteCount();
-              },
-              child: ListView.builder(
-                itemCount: news.length,
-                itemBuilder: (context, index) {
-                  final item = news[index];
-                  final isFavorite =
-                      cubit.favoriteArticleIds.contains(item.articleId);
-
-                  return Container(
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 16),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: const [
-                        BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 4,
-                            offset: Offset(0, 2)),
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ClipRRect(
-                          borderRadius: const BorderRadius.vertical(
-                              top: Radius.circular(20)),
-                          child: item.imageUrl != null
-                              ? Image.network(item.imageUrl!,
-                                  fit: BoxFit.cover,
-                                  width: double.infinity,
-                                  height: screenHeight * 0.25,
-                                  errorBuilder: (context, error, stackTrace) {
-                                  return Center(
-                                    child: SizedBox(
-                                      height: screenHeight * 0.25,
-                                      // color: Colors.red[200],
-                                      child: Center(
-                                        child: const Icon(
-                                            Icons.image_aspect_ratio_outlined,
-                                            color: Colors.black45,
-                                            size: 50),
-                                      ),
-                                    ),
-                                  );
-                                })
-                              : Container(
-                                  height: screenHeight * 0.25,
-                                  color: Colors.grey[200],
-                                  child: const Center(
-                                      child: Text("No Image Available")),
-                                ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(item.title ?? "No title available",
-                                  style: const TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold)),
-                              const SizedBox(height: 8),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    item.pubDate != null
-                                        ? timeago.format(
-                                            DateTime.parse(item.pubDate!),
-                                            allowFromNow: true)
-                                        : "Date not available",
-                                    style: const TextStyle(
-                                        color: Colors.blueGrey, fontSize: 15),
-                                  ),
-                                  IconButton(
-                                    icon: Icon(
-                                      isFavorite
-                                          ? Icons.favorite
-                                          : Icons.favorite_border,
-                                      color:
-                                          isFavorite ? Colors.red : Colors.grey,
-                                    ),
-                                    onPressed: () =>
-                                        cubit.toggleFavorite(item, context),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            );
+            return buildLandscapeGridView(news, cubit, screenHeight, context);
           }
           return const SizedBox.shrink();
         },
---
+++ lib/features/presentation/views/widgets/home_layouts.dart
@@ -0,0 +1,118 @@
+
+import 'package:flutter/material.dart';
+import 'package:news_app/features/data/api/model_api.dart';
+import 'package:path/path.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
+import '../../bloc/fetch_cubit.dart';
+
+
+Widget buildLandscapeGridView(List<NewsData> news, FetchNewsCubit cubit, double screenHeight,  BuildContext context) {
+  final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+  return  RefreshIndicator(
+      onRefresh: () async {
+    await cubit.fetchNews();
+    await cubit.updateFavoriteCount();
+  },
+  child: GridView.builder(
+    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: isPortrait? 1: 2,
+      crossAxisSpacing: 20,
+      mainAxisSpacing: 10,
+      childAspectRatio: 1.2,
+    ),
+    padding: const EdgeInsets.all(20),
+    itemCount: news.length,
+    itemBuilder: (context, index) {
+      final item = news[index];
+      final isFavorite = cubit.favoriteArticleIds.contains(item.articleId);
+
+      return Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: const [
+            BoxShadow(
+                color: Colors.black12,
+                blurRadius: 4,
+                offset: Offset(0, 2)),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: ClipRRect(
+                borderRadius: const BorderRadius.vertical(top: Radius.circular(15)),
+                child: item.imageUrl != null
+                    ? Image.network(
+                    item.imageUrl!,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    errorBuilder: (context, error, stackTrace) {
+                      return Center(
+                        child: Container(
+                          color: Colors.grey[200],
+                          child: const Icon(
+                              Icons.image_aspect_ratio_outlined,
+                              color: Colors.black45,
+                              size: 30),
+                        ),
+                      );
+                    })
+                    : Container(
+                  color: Colors.grey[200],
+                  child: const Center(child: Text("No Image Available")),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.title ?? "No title available",
+                    style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 6),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          item.pubDate != null
+                              ? timeago.format(
+                              DateTime.parse(item.pubDate!),
+                              allowFromNow: true)
+                              : "Date not available",
+                          style: const TextStyle(
+                              color: Colors.blueGrey,
+                              fontSize: 12),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          isFavorite ? Icons.favorite : Icons.favorite_border,
+                          color: isFavorite ? Colors.red : Colors.grey,
+                          size: 20,
+                        ),
+                        onPressed: () => cubit.toggleFavorite(item, context),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    },
+  )
+  );
+}
