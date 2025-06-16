db9bcc895bb91633e7d2ea6f041f9b2e503d80a1
Rijan77 <rijanacharya73@gmail.com>
2025-06-16 17:35:08 +0545

    Extract Data from the api

--- lib/features/data/api/static_api.dart
+++ lib/features/data/api/static_api.dart
@@ -9,9 +9,9 @@ class NewsApi {
   static const String _baseUrl = "https://newsdata.io/api/1/latest";
 
   Future<NewsModel> getNews() async {
-    // final url = Uri.parse("http://10.13.172.119:8081/news");
+    final url = Uri.parse("http://10.13.172.119:8081/news");
 
-    final url = Uri.parse("$_baseUrl?apikey=$_apiKey&q=Hot%20news");
+    // final url = Uri.parse("$_baseUrl?apikey=$_apiKey&q=Hot%20news");
     log("Fetching news from: $url");
 
     final response = await http.get(url);
