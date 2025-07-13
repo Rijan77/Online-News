5ce1f4de52df0919ae20bc8ae868c90e75bcbeb2
Rijan77 <rijanacharya73@gmail.com>
2025-07-13 17:05:08 +0545

    Remove Unused import

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/core/helpers/database_helper.dart';
+import 'package:news_app/features/auth/presentation/views/login.dart';
 import 'package:news_app/features/news/data/repositories/static_api.dart';
 import 'package:news_app/features/news/presentation/blocs/news_fetch_cubit.dart';
 import 'package:news_app/features/auth/presentation/views/registration.dart';
+import 'package:news_app/features/news/presentation/views/home.dart';
 
 import 'core/common/widgets/bottom_nav.dart';
 import 'features/auth/presentation/blocs/auth_cubit/login_cubit.dart';
@@ -41,7 +43,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           initialRoute: '/',
           routes: {
-            '/': (context) => ShowUi(),
+            '/': (context) => Login(),
             '/second': (context) => const Registration(),
             '/third': (context) => const BottomNav(),
           },
