2e64fba91ad05e1791e51826a553692278e4f352
Rijan77 <rijanacharya73@gmail.com>
2025-07-13 15:45:20 +0545

    Update: Folder structure

--- lib/features/news/data/tryDio/dio_api.dart
+++ lib/features/news/data/tryDio/dio_api.dart
@@ -4,36 +4,32 @@ import 'package:news_app/features/news/data/tryDio/dio_model.dart';
 
 class DioApi {
   final Dio dio = Dio();
-  
-  DioApi(){
-    dio.interceptors.add(InterceptorsWrapper(
-      onRequest: (options, handler){
-        print("Send request  to: ${options.uri}");
 
-        options.headers["Authorization"]= "Bearer dummy_token";
-        return handler.next(options);
-      },
-      onResponse: (response, handler){
-        print("Response Recived: ${response.statusCode}");
-        return handler.next(response);
-      },
-      onError: (error, handler){
-        print("Error: ${error.message}");
-        return handler.next(error);
-      }
-    ));
+  DioApi() {
+    dio.interceptors.add(InterceptorsWrapper(onRequest: (options, handler) {
+      print("Send request  to: ${options.uri}");
+
+      options.headers["Authorization"] = "Bearer dummy_token";
+      return handler.next(options);
+    }, onResponse: (response, handler) {
+      print("Response Recived: ${response.statusCode}");
+      return handler.next(response);
+    }, onError: (error, handler) {
+      print("Error: ${error.message}");
+      return handler.next(error);
+    }));
   }
-  
 
   Future<List<DioModel>> fetchData() async {
     try {
       final response =
           await dio.get("https://jsonplaceholder.typicode.com/posts");
 
-      List<DioModel> mapModel = (response.data as List).map((json)=> DioModel.fromJson(json)).toList();
+      List<DioModel> mapModel = (response.data as List)
+          .map((json) => DioModel.fromJson(json))
+          .toList();
 
       return mapModel;
-
     } catch (e) {
       print("Error: $e");
       return [];
